9427c02a1a214b2ceb2683d56b41872348e0c013
DenerNaRaione <energetik19872@gmail.com>
2026-08-29 21:39:54 +0700

    Declare Zepp legacy globals for FlowDiag

--- flowdiag/global.d.ts
+++ flowdiag/global.d.ts
@@ -1,3 +1,8 @@
 import "@zeppos/device-types/dist/index.d.ts";
 import "zosx/zosx_module.d.ts";
 import "zosx/declare_bundle";
+
+declare const hmUI: any;
+declare const hmFS: any;
+declare function App(config: any): void;
+declare function Page(config: any): void;
