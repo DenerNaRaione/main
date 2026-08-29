319dd11cb0b03a97d795701d01431e724f5c1695
DenerNaRaione <energetik19872@gmail.com>
2026-08-29 21:41:30 +0700

    Expose legacy Zepp globals to TypeScript

--- flowdiag/global.d.ts
+++ flowdiag/global.d.ts
@@ -2,7 +2,11 @@ import "@zeppos/device-types/dist/index.d.ts";
 import "zosx/zosx_module.d.ts";
 import "zosx/declare_bundle";
 
-declare const hmUI: any;
-declare const hmFS: any;
-declare function App(config: any): void;
-declare function Page(config: any): void;
+declare global {
+  const hmUI: any;
+  const hmFS: any;
+  function App(config: any): void;
+  function Page(config: any): void;
+}
+
+export {};
